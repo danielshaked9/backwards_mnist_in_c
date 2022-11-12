c1c259849c6c717e7a794ff69824db7c8beb1255
Daniel Shaked <danielshaked9@gmail.com>
2022-11-12 09:08:10 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,15 +13,14 @@ int main(int argc, char **argv)
 {
     Img** imgs = bin_to_imgs("data/train.bin");
     Worker *worker_a=worker_create(784,300,10,0.236);
-    matrix_size(worker_a->net->hidden_weights);
-    //Worker *worker_b=worker_create(10,300,784,0.236);
-    //worker_make(worker_b,imgs,2000);
+    Worker *worker_b=worker_create(10,300,784,0.236);
+    worker_make(worker_b,imgs,1000);
     worker_train(worker_a,imgs,60000);
     imgs_free(imgs,60000);
     imgs = bin_to_imgs("data/test.bin");
     matrix_size(worker_a->net->hidden_weights);
     worker_predict(worker_a,imgs,10000);
-    //worker_show(worker_b,imgs,20);
+    worker_show(worker_b,imgs,20);
     imgs_free(imgs,10000);
     network_free(worker_a->net);
 }
