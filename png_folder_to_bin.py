dc8fed9aef38929810045c8017081f246a6ab2b4
Daniel Shaked <danielshaked9@gmail.com>
2022-11-12 09:07:10 +0200

    Update png_folder_to_bin.py

--- png_folder_to_bin.py
+++ png_folder_to_bin.py
@@ -11,35 +11,33 @@ def search_digit(stringinp):
     for c in stringinp:
         if c.isdigit():
             return c
-path = "test_png"
+path = "train_png/"
 # search text files starting with the word "sales"
 pattern = path + "*.png"
 
 # List of the files that match the pattern
 result = glob.glob(pattern)
 
-
-
-
-output_file = "test.bin"
-out_file = open(output_file,"wb")
-
 count = 0
 for file_name in result:
     count = count + 1
 
+
+
+output_file = "data/train.bin"
+out_file = open(output_file,"wb")
+
+
+
 ds_info=count
 with open(output_file, 'wb') as file:
     file.write(struct.pack('i',count))
-    count=0
 
     for file_name in result:
         kind=int(search_digit(file_name))
         im_frame = Image.open(file_name)
         width=int(im_frame.size[0])
         height=int(im_frame.size[1])
-        
-
         np_frame = np.array(im_frame.getdata()).reshape(im_frame.size)
         file.write(struct.pack('i',width))
         file.write(struct.pack('i',height))
@@ -48,7 +46,41 @@ with open(output_file, 'wb') as file:
         for i in range(height):
             for j in range(width):
                 file.write(struct.pack('d',np_frame[i,j]))
-        print(count ,kind)
-        count+=1
+                
+                
+                
+path = "test_png/"
+# search text files starting with the word "sales"
+pattern = path + "*.png"
+
+# List of the files that match the pattern
+result = glob.glob(pattern)
+
+count = 0
+for file_name in result:
+    count = count + 1
 
 
+
+output_file = "data/test.bin"
+out_file = open(output_file,"wb")
+
+
+
+ds_info=count
+with open(output_file, 'wb') as file:
+    file.write(struct.pack('i',count))
+
+    for file_name in result:
+        kind=int(search_digit(file_name))
+        im_frame = Image.open(file_name)
+        width=int(im_frame.size[0])
+        height=int(im_frame.size[1])
+        np_frame = np.array(im_frame.getdata()).reshape(im_frame.size)
+        file.write(struct.pack('i',width))
+        file.write(struct.pack('i',height))
+        file.write(struct.pack('i',kind))
+
+        for i in range(height):
+            for j in range(width):
+                file.write(struct.pack('d',np_frame[i,j]))
